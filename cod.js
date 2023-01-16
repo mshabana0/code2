b69f82928a3093c0a8ddb89ecf347b8cafceb7ed
mshabana0 <mahamedshabana70@gmail.com>
2023-01-16 06:09:53 +0200

    add new fill

---
+++ cod.js
@@ -0,0 +1,131 @@
+// // let name = 'mahmoud' , age=20;
+// // let salary = 1000 ;
+// // console.log(name)
+// // console.log(age)
+// // console.log(salary)
+
+// const color_red = '#fefef'
+// // // let student_Name='ahmed';
+// // // console.log(student_Name)
+
+// let f =`my name is ${name} `
+// console.log(f)
+//  let q = " what's your name "
+
+// bool
+// let married = false ;
+// let graduated = true ;
+// console.log(married)
+
+// let value = 4 > 5 ; 
+// console.log(value)
+
+// interaction
+
+// let age =prompt('how old are you ?')
+// console.log(age)
+
+// confirm
+
+// let married = confirm('are you married ?')
+// console.log(married)
+
+// alert  (' my name is mohamed ')
+
+// value 
+
+// let value = true ;
+// //  console.log(typeof value)
+
+// console.log(String (value))
+
+// console.log(10%3)
+// console.log(10**3)
+
+// // conditions
+
+// let age = prompt('enter your age :')
+
+// if (age > 20){
+//     console.log('welcome')
+// }else{'end'}
+
+
+// let username ='mahmoud'
+// let password =12345 
+
+// let user_input=prompt('enter user name ;')
+// let password_input = prompt('enter password')
+
+//  if(user_input ==username)
+// { console.log('login succeded')
+// }else{
+//     console.log('login falid')
+// }
+
+// let grade=prompt('enter your garde :')
+
+// if (grade>90){
+//     console.log('A+')
+// } if (grade>80){
+//     console.log('b+')
+// }else  if (grade>70)
+//    { console.log('b+')}
+
+// let allowAccess:
+//  let  age =prompt ('enter your ega')
+// if(age>20)
+// {
+//     allowAccess=true
+// }
+// else
+// {
+//     allowAccess=false
+// }
+// console.log(allowAccess)
+
+// let age =prompt('enter your age')
+
+// let allowAccess=(age>20) ? true : false ;
+
+// // console.log(allowAccess)
+
+// let company = prompt('enter your company :') ;
+// (company=="google") ? alert('good') : alert('bad') 
+
+// logical and or  , or ||
+
+
+
+
+
+
+// let username ='mahmoud'
+// let password =12345 
+
+// let user_input=prompt('enter user name ;')
+// let password_input = prompt('enter password')
+
+//  if(user_input ==username || password_input==password )
+// { console.log('login succeded')
+// }else{
+//     console.log('login falid')
+// }
+
+
+// loops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
